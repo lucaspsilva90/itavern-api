c3985ff9e918a769bc65a368fad4a770e1181330
Lucas Pereira e Silva <lucas.p.silva90@hotmail.com>
2020-12-02 11:58:27 -0300

    proteção dos endpoints não mapeados e tratamento das requisições json

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ type: "*/json" }));
+app.use((err, req, res, next) => {
+    if (SyntaxError) {
+        res.send({ message: "É nessário enviar um json válido no corpo da requisição." })
+        next();
+    }
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
+app.all('*', (req, res) => {
+    res.status(404).send({ message: "Endpoint não encontrado." });
+})
+
 
 module.exports = app;
