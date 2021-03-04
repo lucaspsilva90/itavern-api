46cca9f93d4e78df02b1e082b6d747450fd1031a
lucaspsilva90 <lucas.p.silva90@hotmail.com>
2021-03-04 14:50:27 -0300

    fix config.js heroku

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USERNAME_DEV,
-    password: process.env.DB_PASSWORD_DEV,
-    database: process.env.DB_DATABASE_NAME_DEV,
-    host: process.env.DB_DATABASE_HOST_DEV,
-    dialect: process.env.DB_DATABASE_DIALECT_DEV,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DATABASE_URL,
+    dialect: process.env.DB_DATABASE_DIALECT,
   },
 };
