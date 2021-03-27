6b454fc1942b94de56efbc0246a76cf8ea43569a
lucaspsilva90 <lucas.p.silva90@hotmail.com>
2021-03-27 12:47:57 -0300

    cors fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -45,11 +45,7 @@ module.exports = {
   store: async (req, res) => {
     const userData = req.body;
     const userParametersValidation = userService.objectParametersValidation(userData);
-    res.setHeader('Access-Control-Allow-Origin", "*');
-    res.setHeader('Access-Control-Allow-Credentials", "true');
-    res.setHeader('Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-
+    
     if (userParametersValidation.status) {
       const validationErrors = userParametersValidation.errorFields.toString();
       const singularPhrase = `O campo ${validationErrors}, não existe. Por favor, verifique se o parâmetro fornecido está correto.`;
