75cc881cc28a329fef87e92c6cf787eaea9bfe90
lucaspsilva90 <lucas.p.silva90@hotmail.com>
2021-03-27 12:45:13 -0300

    fix cors response post

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -45,6 +45,10 @@ module.exports = {
   store: async (req, res) => {
     const userData = req.body;
     const userParametersValidation = userService.objectParametersValidation(userData);
+    res.setHeader('Access-Control-Allow-Origin", "*');
+    res.setHeader('Access-Control-Allow-Credentials", "true');
+    res.setHeader('Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
     if (userParametersValidation.status) {
       const validationErrors = userParametersValidation.errorFields.toString();
